9f26f29ee6188515f163b6c0522285d0befdf909
chirupuri99@gmail.com <chirupuri99@gmail.com>
2023-04-17 07:55:39 +0000

    added file_format in source level

--- models/staging/stg_federal_holding.sql
+++ models/staging/stg_federal_holding.sql
@@ -19,4 +19,4 @@ SELECT
     t.$14 AS Change_From_Prior_Week,
     t.$15 AS Change_From_Prior_Year,
     t.$16 AS is_Aggregated
-FROM @{{source('RAW','FED_DATA/fed_holdings.csv')}} (file_format => CSV_FILE_FORMAT) t
+FROM @{{source('RAW','FED_DATA/fed_holdings.csv')}} (file_format => raw.jaffle_shop.CSV_FILE_FORMAT) t
